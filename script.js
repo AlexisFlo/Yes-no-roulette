e1fdb5e1cbe14145efb270c36fdafe470f300ba2
Flowers <floresfernandezalexis05@gmail.com>
2023-10-01 14:44:57 -0600

    Add functionality to spin the arrow and display results in the Roulette game

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
-  const girarBoton = document.getElementById("girar");
-  const resultadoDiv = document.getElementById("resultado");
+  const flecha = document.querySelector(".flecha");
+  const resultado = document.querySelector("#resultado"); // Get the result element
 
-  girarBoton.addEventListener("click", function () {
-      const numeroAleatorio = Math.random();
-      const resultado = numeroAleatorio < 0.5 ? "Sí" : "No";
+  document.addEventListener("click", function () {
+      // Disable the click event while spinning
+      document.body.style.pointerEvents = "none";
 
-      resultadoDiv.textContent = resultado;
+      // Generate a random angle for rotation
+      const angulo = Math.floor(Math.random() * 360);
+
+      // Rotate the arrow with a transition effect
+      flecha.style.transition = "transform 0.5s ease-out";
+      flecha.style.transform = `translate(-50%, -50%) rotate(${angulo}deg)`;
+
+      // Calculate the result based on the angle
+      setTimeout(function () {
+          let textoResultado;
+          if (angulo < 180) {
+              textoResultado = "No";
+          } else {
+              textoResultado = "Sí";
+          }
+
+          // Display the result below the arrow
+          resultado.textContent = `Resultado: ${textoResultado}`;
+
+          // Reset the arrow's rotation and transition effect
+          flecha.style.transition = "none";
+          flecha.style.transform = "translate(-50%, -50%) rotate(0deg)";
+
+          // Enable the click event again
+          document.body.style.pointerEvents = "auto";
+      }, 500); // Adjust the time to match the rotation duration
   });
 });
